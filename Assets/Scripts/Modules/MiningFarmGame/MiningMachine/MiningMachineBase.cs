56a84c0148580721fd7ee2a512ea3d393ba3f2c8
Aleksandr Spiian <aleksandr.spiian@icloud.com>
2025-09-16 17:58:24 +0700

    [Add] Visual stats to MiningMachine

--- Assets/Scripts/Modules/MiningFarmGame/MiningMachine/MiningMachineBase.cs
+++ Assets/Scripts/Modules/MiningFarmGame/MiningMachine/MiningMachineBase.cs
@@ -1,11 +1,16 @@
 using System;
 using MiningFarm.Enums;
 using UnityEngine;
+using TMPro;
 
 namespace MiningFarm.Game
 {
     public abstract class MiningMachineBase : MonoBehaviour
     {
+        [Header("World UI")]
+        [SerializeField] private TextMeshPro _amountText;
+        [SerializeField] private TextMeshPro _progressText;
+        
         public event Action<float, CurrencyType> OnMined;
 
         protected MiningMachineConfig Config;
@@ -21,16 +26,29 @@ namespace MiningFarm.Game
             _miningTime = Config.MiningTimeBase;
             _miningAmount = Config.MiningAmountBase;
             _timer = 0f;
+
+            UpdateUI();
         }
 
         private void Update()
         {
             _timer += Time.deltaTime;
+
+            _progressText.text = $"{_timer:0.00} / {_miningTime:0.00} sec";
+
             if (_timer >= _miningTime)
             {
                 OnMined?.Invoke(_miningAmount, Config.CurrencyType);
+
                 _timer = 0;
+                UpdateUI();
             }
+        }
+
+        private void UpdateUI()
+        {
+            if (_amountText != null)
+                _amountText.text = $"+{_miningAmount} {Config.CurrencyType} per {Config.MiningTimeBase:0.00} sec";
         }
     }
 }
